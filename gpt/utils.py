0efa2912e78f1ebfd1e0387952533718d057a9e4
Jeff Shen <shenjeff@princeton.edu>
2023-12-16 18:10:29 -0500

    add patchify and unpatchify

--- gpt/utils.py
+++ gpt/utils.py
@@ -29,3 +29,32 @@ def _unpatchify(x, channels=1):
         .contiguous()
         .view(channels, n * ps, n * ps)
     )
+
+
+def patchify(x, patch_size):
+    if x.ndim == 3:
+        return _patchify(x, patch_size)
+    if x.ndim == 4:
+        return torch.vmap(_patchify)(x, patch_size=patch_size)
+    else:
+        return NotImplementedError
+
+
+def _patchify(x, patch_size):
+    """Patchify an image."""
+    C, H, W = x.shape
+    ph, pw = patch_size
+    nh, nw = H // ph, W // pw
+    return (
+        rearrange(
+            x,
+            "c (nh ph) (nw pw) -> (nh nw) (ph pw c)",
+            nh=nh,
+            nw=nw,
+            ph=ph,
+            pw=pw,
+            c=C,
+        )
+        .contiguous()
+        .view(nh * nw, ph * pw * C)
+    )
